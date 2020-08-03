adb7ab44dd72886d5bf4523fb565cf9699afca44
Nene888 <68846184+Nene888@users.noreply.github.com>
2020-08-03 11:15:42 +0800

    Add files via upload

---
+++ day1-1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:03:59 2020
+
+@author: USER
+"""
+
+
+print("I'm Eric \nHe is \"cool\"")
+
---
+++ day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:39:13 2020
+
+@author: USER
+"""
+
+name=input("你是誰:\n")
+print(name + ",你好")
