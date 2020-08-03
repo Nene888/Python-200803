9d3ecd1d2b12b3e162da75130749f1fa3b00a933
Nene888 <68846184+Nene888@users.noreply.github.com>
2020-08-03 11:56:49 +0800

    Add files via upload

---
+++ day1-3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 11:24:07 2020
+
+@author: USER
+"""
+
+
+number=input("請輸入半徑:\n")
+
+c=2*3.14 * float(number)
+d=3.14* float(number)* float(number)
+
+print("周長是",c)
+print("面積是",d)
